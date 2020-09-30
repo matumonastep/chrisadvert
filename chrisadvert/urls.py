070174d06f4ac862cd7b4f07ecf78df4cde68f08
matumonastep <matumonastephano@gmail.com>
2020-09-30 11:46:56 -0700

    db connection

--- chrisadvert/urls.py
+++ chrisadvert/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name = 'logout'),
 
-
+    path('profile/', user_views.profile, name = 'profile'),
 
     path('', include('digiadvert.urls')),
 
